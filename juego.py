a3eda7312e085c9fe2fd5cbd314ed976d0722cfe
Jon Landaluce <jonlandaluce000@gmail.com>
2024-02-01 20:44:08 +0100

    test

---
+++ juego.py
@@ -0,0 +1,38 @@
+
+tablero = crea_tablero(6, 7)
+
+# crear 2 jugadores, a los que se asigna una ficha
+
+# ir jugando por turnos
+# input columna
+# jugar con la columna, jugador activo
+
+
+def juega (columna, jugador):
+
+    '''
+    Ver si la columna tiene hueco
+    si lo tiene en ese hueco porner la ficha jugador
+    (Iniciar el juego y elegir columna)
+    '''
+
+def busca_hueco(columna):
+
+    '''
+    Buscar la ultima posicion vacia en la columna del tablero
+    '''
+def busca_hueco(columna):
+
+
+
+tablero = [
+
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+
+]
