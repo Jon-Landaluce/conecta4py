24b121e2d93c03895a7cdc05bea61a6856fcc637
Jon Landaluce <jonlandaluce000@gmail.com>
2024-02-01 22:20:34 +0100

    crear tablero finalizado

--- juego_functions.py
+++ juego_functions.py
@@ -6,3 +6,31 @@ def crea_tablero(fila, columna):
             lista2.append(0)
         salida.append(lista2)
     return salida
+
+def juega(tablero, columna, valor_ficha):
+
+    '''
+    # elegir la columna
+    c = tablero[2]
+    indice = len(c) - 1
+    # recorro la columna de atras a adelante
+    for hueco in c[::-1]:
+    # al encontrar el primer cero, lo sustituyo por valor ficha
+        if hueco == 0:
+            c[indice] = valor_ficha
+            break
+        indice -= 1
+
+    for indice in range(len(c)-1, -1, -1):
+        if c[indice] == 0
+            c[indice] == valor_ficha
+            break
+    '''
+
+    c = tablero[columna]
+    indice = len(c) - 1
+    while indice >= 0:
+        if c[indice] == 0:
+            c[indice] = valor_ficha
+            break
+        indice -= 1
