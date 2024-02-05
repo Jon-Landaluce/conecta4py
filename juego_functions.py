4f0ee5c1ad11e7d9d35fc5e41993ae80a35b7ee0
Jon Landaluce <jonlandaluce000@gmail.com>
2024-02-05 22:45:13 +0100

    Resueltas victorias horizontal y vertical

--- juego_functions.py
+++ juego_functions.py
@@ -1,53 +1,165 @@
 def crea_tablero(fila, columna):
+
     salida = []
+
     for element in range(columna):
+
         lista2 = []
+
         for i in range(fila):
+
             lista2.append(0)
+
         salida.append(lista2)
     return salida
 
-def juega(tablero, columna, valor_ficha):
 
+def juega(tablero, columna, valor_ficha):
     '''
+
     # elegir la columna
+
     c = tablero[2]
+
     indice = len(c) - 1
+
     # recorro la columna de atras a adelante
+
     for hueco in c[::-1]:
+
     # al encontrar el primer cero, lo sustituyo por valor ficha
+
         if hueco == 0:
+
             c[indice] = valor_ficha
+
             break
+
         indice -= 1
 
+
     for indice in range(len(c)-1, -1, -1):
+
         if c[indice] == 0
+
             c[indice] == valor_ficha
+
             break
+
     '''
 
     c = tablero[columna]
+
     indice = len(c) - 1
+
     while indice >= 0:
+
         if c[indice] == 0:
+
             c[indice] = valor_ficha
+
             break
+
         indice -= 1
 
-def esta_llena(tab,columna):
-    
+
+def esta_llena(tab, columna):
+
     # seleccionar columna que queremos comprobar (columna)
+
     # comprobar si la columna tiene huecos con un in
-        # o recprrer la columna posicion a posicion con un bucle
+
+    # o recprrer la columna posicion a posicion con un bucle
 
     c = tab[columna]
+
     '''
+
     if 0 not in c:
+
         return True
     
     else:
+
         return False
+
     '''
+
     return 0 not in c
+
+
+def victoria_vertical_col(tablero, pos_columna, valor_ficha):
+
+    contador_iguales = 0
+
+    columna = tablero[pos_columna]
+
+    indice = 0
+
+    while indice < len(columna):
+
+        if columna[indice] == valor_ficha:
+
+            contador_iguales += 1
+        else:
+
+            contador_iguales = 0
+
+        if contador_iguales == 4:
+
+            return True
+
+        indice += 1
+
+    return False
+
+
+def victoria(tablero, valor_ficha):
+    '''
+    Obtener el numero de columnas de mi tablero
+    Iterar sobre range(num_columnas)
+        para cada columna si es True devolver True
+        si es False pasar a la siguiente
+
+    Si salgo del bucle, devolver False
+    '''
+
+    num_columnas = len(tablero)
+    for num_col in range(num_columnas):
+        if victoria_vertical_col(tablero, num_col, valor_ficha):
+            return True
+
+    '''
+    Obtener el numero de filas
+    iterar sobre range(num_filas)
+        para cada fila si la victoria_horizontal es True, devuelvo True
+        sigo con otro caso
+
+    Si salgo del bucle devolver false
+    '''
+
+    num_filas = len(tablero[0])
+    for num_fila in range(num_filas):
+        if victoria_horizontal_fila(tablero, num_fila, valor_ficha):
+            return True
+
+
+    return False
+
+def victoria_horizontal_fila(tab, pos_fila, valor_ficha):
     
+    contador_iguales = 0
+    
+    for columna in tab:
+        if columna[pos_fila] == valor_ficha:
+            contador_iguales += 1
+        else:
+            contador_iguales = 0
+        if contador_iguales == 4:
+            return True
+
+    return False
+
+
+
+
--- test_juego.py
+++ test_juego.py
@@ -1,22 +1,93 @@
-from juego_functions import crea_tablero, juega, esta_llena
+from juego_functions import crea_tablero, juega, esta_llena, victoria_vertical_col, victoria, victoria_horizontal_fila
+
 
 def test_crear_tablero():
+
     tab = crea_tablero(4, 3)
 
     assert tab == [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
 
+
 def test_anyadir_ficha_a_tablero():
-    tab = crea_tablero(4,3)
 
-    juega(tab, 2, 1)
+    tab = crea_tablero(4, 3)
 
-    assert tab == [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 1]]
+    juega(tab, 2, 'x')
+
+    assert tab == [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 'x']]
+
+    juega(tab, 1, 'o')
+
+    assert tab == [[0, 0, 0, 0], [0, 0, 0, "o"], [0, 0, 0, 'x']]
+
 
 def test_comprobar_columna_llena():
-    tab = crea_tablero(4,3)
+
+    tab = crea_tablero(4, 3)
     juega(tab, 2, 'x')
     juega(tab, 2, 'x')
     juega(tab, 2, 'x')
     juega(tab, 2, 'x')
 
     assert esta_llena(tab, 2) == True
+
+
+def test_victoria_vertical_columna():
+
+    tab = crea_tablero(6, 7)
+
+    assert victoria_vertical_col(tab, 2, "x") == False
+    juega(tab, 2, 'x')
+    juega(tab, 2, 'o')
+    juega(tab, 2, 'x')
+    assert victoria_vertical_col(tab, 2, "x") == False
+    juega(tab, 2, 'x')
+    assert victoria_vertical_col(tab, 2, "x") == False
+    juega(tab, 2, 'x')
+    assert victoria_vertical_col(tab, 2, "x") == False
+    juega(tab, 2, 'x')
+    assert victoria_vertical_col(tab, 2, "x") == True
+
+
+def test_victoria_vertical_tablero():
+
+    tab = crea_tablero(6, 7)
+    assert victoria(tab, 'x') == False
+
+    juega(tab, 2, 'x')
+    juega(tab, 2, 'o')
+    juega(tab, 2, 'x')
+    juega(tab, 2, 'x')
+    juega(tab, 2, 'x')
+    juega(tab, 2, 'x')
+
+    assert victoria(tab, 'x')
+    assert victoria(tab, 'o') == False
+
+def test_victoria_horizontal_fila():
+
+    tab = crea_tablero(6, 7)
+    
+    juega(tab, 0, 'x')
+    juega(tab, 1, 'o')
+    juega(tab, 2, 'x')
+    juega(tab, 3, 'x')
+    juega(tab, 4, 'x')
+    juega(tab, 5, 'x')
+
+    assert victoria_horizontal_fila(tab, 5, 'x')
+
+
+def test_victoria_horizontal_tablero():
+
+    tab = crea_tablero(6, 7)
+
+    juega(tab, 0, 'x')
+    juega(tab, 1, 'o')
+    juega(tab, 2, 'x')
+    juega(tab, 3, 'x')
+    juega(tab, 4, 'x')
+    juega(tab, 5, 'x')
+
+    assert victoria(tab, 'x') == True
+    assert victoria(tab, 'o') == False
